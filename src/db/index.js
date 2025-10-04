32a441c599dd7fa40a87b99df97660f0150bb4c2
Prvncodz <praveenwks@gmail.com>
2025-10-04 18:55:21 +0530

    db connection in mern

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
-import {DB_NAME} from ../constants.js;
+import {DB_NAME} from "../constants.js";
 
 const dbConn=(async ()=>{
    try{
-   const conn=mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+   const conn= await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
     console.log("mongodb connection successfull");
-    console.log(`connection host:${conn.connection.host}`);
-
+    console.log("Host :",conn.connection.host);
+    
 } catch(error){
    console.error(`ERROR: ${error}`);
 }
 }
 )()
+export default dbConn;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+import dbConn from "./db/index.js";
+
+
+
