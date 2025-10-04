bca972899fa2f922b950a925c5e166170fc63e7a
Prvncodz <praveenwks@gmail.com>
2025-10-04 22:29:54 +0530

    g

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import dbConn from "./db/index.js";
 import app from "./app.js"
-
+import express from "express"
 dbConn
 .then(()=>{
   app.listen(process.env.PORT || 8000,()=>{
