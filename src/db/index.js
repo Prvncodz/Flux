e75a8c8cc4f0ab5464b6cb6e277dbcf7bf68cff6
Prvncodz <praveenwks@gmail.com>
2025-10-04 18:02:49 +0530

    changes

--- src/db/index.js
+++ src/db/index.js
@@ -5,6 +5,8 @@ const dbConn=(async ()=>{
    try{
    const conn=mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
     console.log("mongodb connection successfull");
+    console.log(`connection host:${conn.connection.host}`);
+
 } catch(error){
    console.error(`ERROR: ${error}`);
 }
