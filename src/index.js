8816f77a15ddaf35f5ed0175bdea3e3b1e95cb29
Prvncodz <praveenwks@gmail.com>
2025-10-04 20:35:00 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import dotenv from "dotenv";
 import dbConn from "./db/index.js";
+import app from "./app.js"
 
+dbConn
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`app is running at port: ${process.env.PORT}`);
+});
+  app.on("error",()=>{
+   consoe.log("app failed to run !!");
+});
+})
 
-
+.catch((err)=>{
+   console.log(`mongodb connection failed !!!,error: ${err}`);
+})
