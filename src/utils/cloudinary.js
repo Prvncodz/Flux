827b75b619be7da7cba103eebb79c56821b2439c
Prvncodz <praveenwks@gmail.com>
2025-10-08 22:36:09 +0530

    changes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const uploadOnCloud=async (filePath)={
   try{
   if(!filePath)return null;
-  const response= cloudinary.uploader.upload(filePath,()={
+  const response= await cloudinary.uploader.upload(filePath,()={
                   resource_type:"auto"
 })
 
