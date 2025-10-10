fb23ae48c80e21b426114e2f713564da3b57e52b
Prvncodz <praveenwks@gmail.com>
2025-10-10 11:53:53 +0530

    tested post api of resgister user with teste

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,18 @@
 import {Router} from "express"
 import {registerUser} from "../controllers/user.controller.js"
-
+import {upload} from "../middlewares/multer.js"
 const router=Router()
-router.post("/register",registerUser)
+router.post("/register"
+,upload.feilds(
+               {
+		name:"avatar",
+ 		maxCount:1
+   		}
+	      {
+		name:"coverImage",
+                maxCount:1
+		}
+   	      )
+,registerUser)
 
 export default router
