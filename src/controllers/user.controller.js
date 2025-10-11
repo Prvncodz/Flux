7478f3c96814da359fe99fa656084e5a466c3ab9
Prvncodz <praveenwks@gmail.com>
2025-10-11 15:48:03 +0530

    user info and validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,24 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
-
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
 const  registerUser=asyncHandler(async(req,res)=>{
-     res.status(200).json({message:"ok"})
+        // we will get the user info
+        // validate the info
+       //check is user already exists - username
+        // check for images
+        // if there is an image for avatar upload to cloudinary
+        // make user object and store in db
+        // while removing the pass and refreshTokens because we dont want to send them in the response.
+        // return res
+ const {fullName,userName,email}=req.body
+  console.log("email :",email)
+if([fullName,userName,email].some((feild) =>feild?.trim()==="")){
+  throw new ApiError(400,"All feilds are required");
+}
+ 
+  const existsUser=User.findOne({userName});
+  if(existsUser)throw new ApiError(402,"user already exists");
+  
 })
 
 export {registerUser}
