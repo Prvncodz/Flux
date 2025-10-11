d1bb13b861b510c85277f8e260b2946e6b040dc6
Prvncodz <praveenwks@gmail.com>
2025-10-11 16:46:38 +0530

    user registration and bug handling done according to the algorithm

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,10 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
+import {router} from "../routes/user.routes.js"
+import {upload} from "../middlewares/multer.js"
+import {uploadOnCloud} from "../utils/clodinary.js"
+
 const  registerUser=asyncHandler(async(req,res)=>{
         // we will get the user info
         // validate the info
@@ -10,15 +14,41 @@ const  registerUser=asyncHandler(async(req,res)=>{
         // make user object and store in db
         // while removing the pass and refreshTokens because we dont want to send them in the response.
         // return res
- const {fullName,userName,email}=req.body
-  console.log("email :",email)
+
+ const {fullName,userName,password,email}=req.body
+
+
 if([fullName,userName,email].some((feild) =>feild?.trim()==="")){
   throw new ApiError(400,"All feilds are required");
 }
- 
+
   const existsUser=User.findOne({userName});
   if(existsUser)throw new ApiError(402,"user already exists");
-  
+
+  const avatarLocalPath=req.files?.avatar[0];
+ const coverImageLocalPath=req.files?.coverImage[0];
+ if(!avatarLocalPath){
+   throw new ApiError(403,"avatar is required to register"
+}
+   // we should use await while the file get uploaded to the cloud from the local path
+   const avatar= await uploadOnCloud(avatarLocalPath)
+   const coverImage=coverImageLocalPath? await uploadOnCloud(coverImageLocalPath)
+   if(!avatar){
+throw new ApiError(403,"avatar is required to register"
+}
+   const user=User.create(
+    userName:userName.toLowerCase(),
+    fullName,
+    email,
+    avatar:avatar,
+    coverImage:coverImage?coverImage||"",
+    password
+)
+   const createdUser=User.findById(user._id).select(" -password -refreshTokens")
+   if(!createdUser){
+   throw new ApiError(500,"Unable to register user")
+  }
+    return res.status(200).json(ApiResponse(203,createdUser,"User registered successfully"))
 })
 
 export {registerUser}
