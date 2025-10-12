2f70665227e795c7616b13b1e67814b456a69b55
Prvncodz <praveenwks@gmail.com>
2025-10-12 17:22:51 +0530

    cloudinary bugs fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,7 @@ import {router} from "../routes/user.routes.js"
 import {upload} from "../middlewares/multer.js"
 import {uploadOnCloud} from "../utils/cloudinary.js"
 
-const  registerUser=asyncHandler(async(req,res)=>{
+const  registerUser=asyncHandler(async (req,res)=>{
         // we will get the user info
         // validate the info
        //check is user already exists - username
@@ -29,17 +29,13 @@ throw new ApiError(402,"user already exists");
 
   const avatarLocalPath=req.files?.avatar?.[0]?.path;
  const coverImageLocalPath=req.files?.coverImage?.[0]?.path;
-  console.log(req.files);
-  console.log(req.files?.avatar[0]); 
-  console.log("avatar local path :",avatarLocalPath);
  
 if(!avatarLocalPath){
-   throw new ApiError(403,"avatar is required to register");
-}
-   // we should use await while the file get uploaded to the cloud from the local path
+   throw new ApiError(403,"avatar file path is required to register");
+}   // we should use await while the file get uploaded to the cloud from the local path
    const avatar= await uploadOnCloud(avatarLocalPath)
-   console.log("avatar check :",avatar);
-  const coverImage=await uploadOnCloud(coverImageLocalPath);
+ 
+  const coverImage=await uploadOnCloud(coverImageLocalPath)
    if(!avatar){
  throw new ApiError(403,"avatar is required to register");
 }
