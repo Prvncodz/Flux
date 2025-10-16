9a2a6d49883ea8a68b155c029247e343e5ba98c1
Prvncodz <praveenwks@gmail.com>
2025-10-16 15:38:06 +0530

    Added change password logic in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -179,4 +179,21 @@ const refreshAccessTokens=asyncHandler(async(req,res)=>{
 
 })
 
-export {registerUser,loginUser,logoutUser}
+//change password
+
+const changePassword=asyncHandler(async(req,res)=>{
+  const {oldPassword,newPassword}=req.body
+  const user=await User.findById(req.user?_id)
+  const isPasswordCorrect= await user.isPasswordCorrect(oldPassword)
+  if(!isPasswordCorrect){
+    throw new ApiError(400,"wrong password")
+  }
+   user.password=newPassword
+   await user.save({validateBeforeSave:false})
+   return res
+    .status(200)
+    .json(new ApiResponse(200,"Your password is changed successfully"));
+})
+
+
+export {registerUser,loginUser,logoutUser,refreshAccessTokens}
