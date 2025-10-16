844cc8592f62071df38899ae682d2d719f462f57
Prvncodz <praveenwks@gmail.com>
2025-10-16 11:34:34 +0530

    made an endpoint for refresh tokens

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,18 @@
 import {Router} from "express"
-import {registerUser} from "../controllers/user.controller.js"
+import {registerUser,loginUser,logoutUser,refreshAccessTokens} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.js"
+import { verifyJwt } from "../middlewares/auth.js"
+
 const router=Router();
 router.post("/register"
 ,upload.fields([
        {name:"avatar",maxCount:1},
  	{name:"coverImage",maxCount:1},
    	     ] ),registerUser);
-router.get("/login",loginUser);
-export { router } ;
+
+router.post("/login",loginUser);
+
+//secured routes
+router.post("/logout",verifyJwt,logoutUser);
+router.post("/refreshTokens",refreshAccessTokens)
+export default router ;
