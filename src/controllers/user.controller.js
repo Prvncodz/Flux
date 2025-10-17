fa4448c9de94683ef8636977d0309111e5c5cf6e
Prvncodz <praveenwks@gmail.com>
2025-10-17 19:05:38 +0530

    learnt abour aggregation piple line today,made a controller to fetch user profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -296,6 +296,66 @@ const updateUserCoverImage=asyncHandler(async(req,res)=>{
     .json(new ApiResponse(200,updateCoverImage,"cover image updated successfully "));
 
 })
-
+   const showUserProfile=asyncHandler(async(req,res)=>{
+  const {username}=req.params
+  if(!username?.trim()){
+    throw new ApiError(400,"username is missing")
+  }
+  const channel =await User.aggregiate([
+    {
+      $match:{
+        userName:username?.toLowerCase()
+      }
+    },
+    {
+      $lookup:{
+        from:"subscriptions"
+        localField:"_id",
+        foriegnField:"channel",
+        as:"subscribers"
+      }
+    },
+    {
+      $lookup:{                                    from:"subscriptions"     
+       localField:"_id",        
+        foriegnField:"subscriber",                      as:"subscribed"
+      }
+    },
+    {
+      $addFieds:{
+        subscriberCount:{
+          $size:"subscribers"
+        },
+        channelsSubscribedCount:{
+        $size:"subscribed"
+      },
+        isSubscribed:{
+         $cond:{
+            if:{$in[req.user?_id,"$subscribed.subscriber"]},
+            then:true,
+            else:false
+          }
+       }
+      }
+    },{
+       $project:{
+        fullName:1,
+        userName:1,
+        avatar:1,
+        coverImage:1,
+        subscriberCount:1,
+        channelsSubscribedCount:1,
+        isSubscribed:1
+      }
+    }
+  ])
+  console.log(channel)
+ if (!channel?.length) {
+    throw new ApiError(404,"channel does not exist")
+ }
+  return res
+  .status(200)
+  .json(new ApiResponse(200,channel.[0],"user profile fetched successfully"))
+})
 
 export {registerUser,loginUser,logoutUser,refreshAccessTokens,changePassword,currentUser,updateAccountInfo,updateUserAvatar,updateUserCoverImage}
