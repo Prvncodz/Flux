c854776b209baa029bc49815554540888b226da1
Prvncodz <praveenwks@gmail.com>
2025-10-18 19:58:33 +0530

    added like mongoose  model

---
+++ src/models/like.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const likeSchema= new mongoose.Schema({
+  comment:{
+    type:mongoose.Types.ObjectId,
+    ref:"Comment"
+  },
+  video:{
+    type:mongoose.Types.ObjectId,                  ref:"Video"
+  },
+  tweet:{
+    type:mongoose.Types.ObjectId,                   ref:"Tweet"
+  },⅝
+  likedBy:{
+    type:mongoose.Types.ObjectId,                   ref:"User"
+  },
+},{timestamps:true})
+
+export const Like=mongoose.model("Like",likeSchema)
