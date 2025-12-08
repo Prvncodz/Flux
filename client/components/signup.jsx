97ac25323a8033d99dc8c8f2b7f8e41cee0677b2
Prvncodz <praveenwks@gmail.com>
2025-12-08 09:37:08 +0530

    Added some Design changes and error handling for frontend

--- client/components/signup.jsx
+++ client/components/signup.jsx
@@ -1,17 +1,25 @@
 import defaultPfp from "./assets/dpfp.jpg"
 import defaultBanner from "./assets/dbanner.jpg"
+import editIcon from "./assets/editimage.png"
 import {useState,useEvent,useRef} from 'react'
 import axios from "axios"
 
 export default function SignUp(){
 	const [coverImagePreview,setCoverImagePreview]=useState(null)
 	const [avatarPreview,setAvatarPreview]=useState(null)
+	const [DisplayAvatarRequired,setDisplayAvatarRequired]=useState(false)
 	const fileRefci=useRef(null)
 	const fileRefav=useRef(null)
+
 	
 	function handleFormSubmission(e){
 		e.preventDefault()
 		const formData=new FormData(e.target)
+	 const avatar=formData.get("avatar");
+	 if(!avatar || avatar.size==0){
+	 	setDisplayAvatarRequired(true)
+	 	return
+	 }
 	  try{
 		const res=axios.post("http://localhost:8000/api/v1/user/register",formData,{
 			headers:{
@@ -20,9 +28,11 @@ export default function SignUp(){
 		})
 		if(res.ok){
 		alert("Success");
+		}else{
+   alert("Server error");
 		}
 	  }catch(err){
-	  	alert("Error :",err.message)
+	  	alert("Error while submitting the form")
 	  	e.target.reset();
 	  }
 	  e.target.reset();
@@ -39,19 +49,22 @@ export default function SignUp(){
 	function handleAvatar(e){
 		const file=e.target.files[0]
 		if(file){
+		setDisplayAvatarRequired(false)
 		setAvatarPreview(URL.createObjectURL(file))
 		}
 	}
 	
 	return(
-		<div id="signup-bg" className="h-screen w-screen flex justify-center items-center bg-gray-300">
+		<div id="signup-bg" className="h-screen w-screen flex flex-col justify-center
+		items-center bg-gray-200">
 			<div id="signup-card" className="h-auto w-90 bg-gray-100 flex flex-col
 			justify-center overflow-hidden rounded-xl">
-				<h1 className="mt-7 text-2xl font-extrabold text-blue-400">Sign Up</h1>
+				<h1 className="mt-5 text-3xl font-bold text-blue-400 relative" >Sign Up</h1>
 				
 				
 				<form className="p-7" onSubmit={handleFormSubmission}>
-			 <div className="wrapper relative">
+			 <div className="wrapper relative transition-all ease">
+			 	<div>
 			 	<img src={coverImagePreview?coverImagePreview:defaultBanner}
 			 	onClick={()=>{
 			 	 	fileRefci.current.click();
@@ -59,37 +72,49 @@ export default function SignUp(){
 			 	className="h-33
 			 	w-full rounded-lg relative cursor-pointer"/>
 			 	
+			 	
 			 	<input type="file" ref={fileRefci} className="hidden" name="coverImage"
 			 	accept="image/*" onChange={handleCoverImage}/>
 			 
-			 
+			 </div>
+			 <div className="relative">
 			 	<img src={avatarPreview?avatarPreview:defaultPfp}
 			 	onClick={()=>{
 			 	 	fileRefav.current.click();
 			 	 }} 
-			 	className="h-15
-			 	w-15 rounded-full absolute -left-1 -bottom-3 cursor-pointer"/>
-			 	
+			 	className={`h-15
+			 	w-15 rounded-full absolute -left-1 -bottom-3 cursor-pointer
+			  z-1
+			 	${DisplayAvatarRequired?'border-2 border-red-400':''}`}  />
+			 <div className="absolute z-2 bg-gray-800 transparent h-15 w-15 rounded-full -left-1
+			 -bottom-3"></div>	
+			 <img src={editIcon} className="absolute h-12 w-13 -bottom-1 left-0 z-3"/>
+			 
+			 	{ DisplayAvatarRequired?(<div className="bg-gray-100 rounded-sm h-5 w-auto
+			 	ml-2 mt-2 text-red-400">Avatar is required to register</div>):(<p></p>)}
 			 	<input type="file" ref={fileRefav} className="hidden" name="avatar"
 			 	accept="image/*" onChange={handleAvatar}/>
+			 	</div>
 			 </div>
 			 
 			<div className="form-inputs mt-10 mb-5  h-auto w-full relative
 			text-left">
-			<label className="text-lg font-medium">Fullname<input name="fullName"
-			type="text" className="bg-gray-200
-		 w-full  mt-1 mb-4 rounded-md p-1 border border-gray-300" required /></label>
-			<label className="text-lg font-medium">Username<input name="userName"
-			type="text" className="bg-gray-200 w-full  mb-4 rounded-md p-1 border
-			border-gray-300 mt-1" required/></label>
-			<label className="text-lg font-medium">Email<input name="email" type="email"
-			className="bg-gray-200 w-full mb-4 rounded-md p-1 border border-gray-300 mt-1" required/></label>
-			<label className="text-lg font-medium">Password<input name="password"
-			type="password" className="bg-gray-200 w-full mb-4 rounded-md p-1 border
-			border-gray-300 mt-1" required/></label> 
+			<label className="text-lg font-medium text-gray-700">Fullname<input name="fullName"
+			type="text" className="bg-gray-100
+		 w-full  mt-1 mb-4 rounded-md p-1 border border-gray-200 shadow-xs" required /></label>
+			<label className="text-lg font-medium text-gray-700">Username<input name="userName"
+			type="text" className="bg-gray-100 w-full  mb-4 rounded-md p-1 border
+			border-gray-200 shadow-xs mt-1" required/></label>
+			<label className="text-lg font-medium text-gray-700">Email<input name="email" type="email"
+			className="bg-gray-100 w-full mb-4 rounded-md p-1 border border-gray-200
+			shadow-xs mt-1" required/></label>
+			<label className="text-lg font-medium text-gray-700">Password<input name="password"
+			type="password" className="bg-gray-100 w-full mb-4 rounded-md p-1 border
+			border-gray-200 shadow-xs mt-1 " required/></label> 
 		</div>
-		  <button className="bg-blue-500 hover:bg-blue-600 focus:outline-2
-		  focus:outline-offset-2 focus:outline-blue-500  active:bg-blue-700 text-gray-200 p-5 w-44 h-12 ml-auto mr-auto
+		  <button className="bg-blue-500 hover:bg-blue-600 
+		  focus:outline-offset-2 active:bg-blue-800
+		  text-gray-200 p-5 w-44 h-12 ml-auto mr-auto
 		 flex justify-center items-center rounded-4xl
 		  font-semibold text-center text-lg "
 		  type="submit">Submit</button>
