d5ef041bbfa50bc9145f47b92964f05bebe42015
prvncodz <praveenwks@gmail.com>
2026-01-01 18:04:54 +0530

    Added get user avatar hook

--- client/hooks/useGetUserAvatar.jsx
+++ client/hooks/useGetUserAvatar.jsx
@@ -1,24 +1,30 @@
 import { useEffect,useState } from "react";
 
- export  funtion useGetUserAvatar(userId)=>{
-      
+ export  funtion useGetUserAvatar(userId)=>{ 
       const [url,setUrl]=useState(null)
+      const [loading,setLoading]=useState(false)
+
      if(!userId ){
     return null
      }
+
       useEffect(()=>{
   
         try {
-  
+         setLoading(true)
          const res = await axios.get(`user/c/${userId}`)
           if(res.status===200){  
           console.log("res data :",res.data.data.avatar.url)
-          setUrl()
+          setUrl(res.data.data.avatar.url)
+          setLoading(false)
       }
         } catch (error) {
            console.log(error)
            console.log("backend message :",error?.response?.data?.message)
-            return null
+            setLoading(false)
+
         }
 },[])
+
+ return loading?null : url
 }
