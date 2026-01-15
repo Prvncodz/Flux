eb71fe9679f88036fae954fafffef5bd96302140
prvncodz <praveenwks@gmail.com>
2026-01-15 15:25:39 +0530

    Made comment component

---
+++ client/components/comments/commentComponent.jsx
@@ -0,0 +1,34 @@
+import { useGetUserById } from "../../hooks/useGetUserById.jsx";
+import Like from "../home/likeComponent/tweetlike.jsx"
+import dpfp from "../assets/dpfp.jpg"
+
+
+export default function CommentComponent({ comment, idx }) {
+  const { avatarUrl, fullname } = useGetUserById(comment?.owner);
+
+  return (
+    <>
+      <div className=" h-auto w-full p-3 border-b border-gray-300 mt-0 mb-0 ">
+        <div className="flex mt-3">
+          <div className="h-10 w-10 ml-4">
+            <img
+              src={avatarUrl || dpfp}
+              className="rounded-full h-10 w-10"
+              onError={(e) => e.target.src = dpfp}
+            />
+          </div>
+          <span className="ml-4 h-7">
+            <h3 className="text-left text-neutral-700 font-medium text-lg">{fullname}</h3>
+          </span>
+
+        </div>
+        <div className="pt-4 pl-4 h-auto w-full wrap-break-word text-neutral-700 text-body font-medium text-left ">
+          {comment?.content || ""}
+        </div>
+        <div className="flex justify-start gap-6 mt-4 ml-5">
+          <span><Like commentId={comment?._id} /></span>
+        </div>
+      </div>
+    </>
+  );
+}
