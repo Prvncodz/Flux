ae4f98d8d277031eb227f070dffe5061b1825b36
Prvncodz <praveenwks@gmail.com>
2026-03-03 16:17:10 +0530

    finished video upload popup

--- client/components/submitButton.jsx
+++ client/components/submitButton.jsx
@@ -4,7 +4,7 @@ export default function SubmitButton({ currentSubmitStatus, text, className }) {
       className={`bg-blue-500 hover:bg-blue-600 
 		  focus:outline-offset-2 active:bg-blue-800
 		  text-gray-100 p-5 w-38 h-11 ml-auto mr-auto
-		 flex justify-center items-center rounded-4xl 
+		 flex justify-center items-center rounded-full 
 		  font-semibold text-center text-md transition-bg ease cursor-pointer ${className}`}
       type="submit"
     >
--- client/components/uploadPopup/videoUpload.jsx
+++ client/components/uploadPopup/videoUpload.jsx
@@ -5,6 +5,10 @@ import SubmitButton from "../submitButton.jsx";
 export default function VideoUploadPopup({ setShowPopup }) {
   const [loading, SetLoading] = useState(false);
   const [isSubmmited, setIsSubmmited] = useState(false);
+  const [fileName, setFileName] = useState(("Select video file"))
+  function handleVideoFile(fname) {
+    setFileName(fname);
+  }
 
   return (
     <>
@@ -12,8 +16,8 @@ export default function VideoUploadPopup({ setShowPopup }) {
         <h1 className="mt-5 mb-3 text-xl font-medium text-blue-400 relative">
           Upload Video
         </h1>
-        <div className="flex items-center justify-center h-40 w-full px-4 py-0 my-2 ">
-          <div className="border-2 border-dotted border-gray-400 h-full w-full rounded-2xl flex justify-center items-center flex-col">
+        <div className="flex items-center justify-center h-50 w-full px-4 py-0 my-2 ">
+          <label htmlFor="for-upload" className="border-2 border-dotted border-gray-400 h-full w-full flex justify-center items-center flex-col">
             <svg
               width="50" height="50" viewBox="0 0 24 24"
               fill="none" stroke="#111" strokeWidth="1.5"
@@ -22,17 +26,30 @@ export default function VideoUploadPopup({ setShowPopup }) {
               <rect x="1" y="5" width="15" height="14" rx="2" />
               <polygon points="23 7 16 12 23 17 23 7" />
             </svg>
-            <h3 className="text-sm text-gray-400 mt-2">Upload files here </h3>
-          </div>
+            <h3 className="text-sm text-gray-400 mt-2">{fileName}</h3>
+            <input type="file" name="videofile" onChange={(e) => handleVideoFile(e.target?.files[0]?.name)} className="hidden" id="for-upload" />
+          </label>
         </div>
-        <div className="px-4 mt-4">
-          <label className="text-md font-base text-gray-700 ">
+
+        <div className="px-4 mt-4 text-left flex flex-col gap-6">
+          <label className="text-md font-base text-gray-700">
+            Thumbnail*
+            <input
+              name="thumbnail"
+              type="file"
+              className="bg-gray-100
+                w-full  p-1  text-base file:mx-2 file:rounded-full file:border-0 file:bg-violet-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-blue-700 hover:file:bg-blue-100 dark:file:bg-blue-600 dark:file:text-blue-100 dark:hover:file:bg-blue-500 "
+            />
+          </label>
+
+          <label className="text-md font-base text-gray-700">
             Title
             <input
               name="title"
               type="text"
+              placeholder="Title of the video goes here..."
               className="bg-gray-100
-                w-full  mt-1 mb-4 rounded-md p-1 shadow-md border border-gray-300"
+                w-full py-2 px-2 shadow-md placeholder:text-sm"
             />
           </label>
 
@@ -41,14 +58,18 @@ export default function VideoUploadPopup({ setShowPopup }) {
             <textarea
               name="description"
               type="text"
+              placeholder="description of the video goes here.."
               className="bg-gray-100
-                h-30 w-full  mt-1 mb-4 rounded-md p-1 shadow-md"
+                h-30 w-full p-2 shadow-md text-sm"
             />
           </label>
         </div>
-        <SubmitButton currentSubmitStatus={
-          isSubmmited ? "submited" : loading ? "loading" : "normal"
-        } text={"Upload"} className={"mt-2 mb-5"} />
+        <div className="flex items-center justify-center">
+          <button type="button" onClick={() => setShowPopup(false)} className="bg-gray-300 text-base font-semibold text-gray-800 p-4 flex items-center justify-center text-center rounded-full w-38 h-11 mt-2 mb-5 mx-auto" >Cancel</button>
+          <SubmitButton currentSubmitStatus={
+            isSubmmited ? "submited" : loading ? "loading" : "normal"
+          } text={"Upload"} className={"mt-2 mb-5"} />
+        </div>
       </PopUpComponent>
     </>
   );
