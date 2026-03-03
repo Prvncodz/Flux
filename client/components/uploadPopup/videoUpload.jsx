3c5fa72916f226027197cf49bf8b8892f1ac3c7a
Prvncodz <praveenwks@gmail.com>
2026-03-03 16:59:05 +0530

    UI changes

--- client/components/uploadPopup/videoUpload.jsx
+++ client/components/uploadPopup/videoUpload.jsx
@@ -38,8 +38,8 @@ export default function VideoUploadPopup({ setShowPopup }) {
           <h1 className="mt-5 mb-3 text-xl font-medium text-blue-400 relative">
             Upload Video
           </h1>
-          <div className="flex items-center justify-center h-50 w-full px-4 py-0 my-2 ">
-            <label htmlFor="for-upload" className="border-2 border-dotted border-gray-400 h-full w-full flex justify-center items-center flex-col">
+          <div className="flex items-center justify-center h-40 w-full px-4 py-0 my-2 ">
+            <label htmlFor="for-upload" className="border-2 border-dotted border-gray-300 h-full w-full flex justify-center items-center flex-col">
               <svg
                 width="50" height="50" viewBox="0 0 24 24"
                 fill="none" stroke="#111" strokeWidth="1.5"
@@ -60,7 +60,7 @@ export default function VideoUploadPopup({ setShowPopup }) {
                 name="thumbnail"
                 type="file"
                 className="bg-gray-100
-                w-full  p-1  text-base file:mx-2 file:rounded-full file:border-0 file:bg-violet-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-blue-700 hover:file:bg-blue-100 dark:file:bg-blue-600 dark:file:text-blue-100 dark:hover:file:bg-blue-500 "
+                w-full  p-1  border-graytext-base file:mr-2 file:rounded-full file:border-0 file:bg-violet-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-blue-700 hover:file:bg-blue-100 dark:file:bg-blue-600 dark:file:text-blue-100 dark:hover:file:bg-blue-500 "
               />
             </label>
 
@@ -71,7 +71,7 @@ export default function VideoUploadPopup({ setShowPopup }) {
                 type="text"
                 placeholder="Title of the video goes here..."
                 className="bg-gray-100
-                w-full py-2 px-2 shadow-md placeholder:text-sm"
+                w-full py-2 px-2 border border-gray-300 placeholder:text-sm"
               />
             </label>
 
@@ -82,11 +82,11 @@ export default function VideoUploadPopup({ setShowPopup }) {
                 type="text"
                 placeholder="description of the video goes here.."
                 className="bg-gray-100
-                h-30 w-full p-2 shadow-md text-sm"
+                h-30 w-full p-2 border border-gray-300 text-sm"
               />
             </label>
           </div>
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center mt-1">
             <button type="button" onClick={() => setShowPopup(false)} className="bg-gray-300 text-base font-semibold text-gray-800 p-4 flex items-center justify-center text-center rounded-full w-38 h-11 mt-2 mb-5 mx-auto" >Cancel</button>
             <SubmitButton currentSubmitStatus={
               isSubmmited ? "submited" : loading ? "loading" : "normal"
