97fdbd06ac6cf8fc1cfbaf3e8537936c909081de
Prvncodz <praveenwks@gmail.com>
2026-03-13 08:01:42 +0530

    ui changes

--- client/components/uploadPopup/videoUpload.jsx
+++ client/components/uploadPopup/videoUpload.jsx
@@ -36,7 +36,7 @@ export default function VideoUploadPopup({ setShowPopup }) {
     <>
       <PopUpComponent onCancel={() => setShowPopup(false)}>
         <form onSubmit={(e) => handleVideoUpload(e)}>
-          <h1 className="mt-5 mb-3 text-xl font-medium text-blue-500 relative">
+          <h1 className="mt-5 mb-3 text-xl font-medium text-[#0A98FC] relative">
             Upload Video
           </h1>
           <div className="flex items-center justify-center h-40 w-full px-4 py-0 my-2 ">
@@ -61,7 +61,7 @@ export default function VideoUploadPopup({ setShowPopup }) {
                 name="thumbnail"
                 type="file"
                 className="bg-gray-100
-                w-full  p-1  border-graytext-base file:mr-2 file:rounded-full file:border-0 file:bg-violet-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-blue-700 hover:file:bg-blue-100 dark:file:bg-blue-600 dark:file:text-blue-100 dark:hover:file:bg-blue-500 "
+                w-full  p-1  border-graytext-base file:mr-2 file:rounded-full file:border-0 file:bg-yellow-400 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-blue-700 hover:file:bg-blue-100 dark:file:bg-blue-600 dark:file:text-blue-100 dark:hover:file:bg-blue-500 "
               />
             </label>
 
@@ -70,9 +70,8 @@ export default function VideoUploadPopup({ setShowPopup }) {
               <input
                 name="title"
                 type="text"
-                placeholder="Title of the video goes here..."
                 className="bg-gray-100
-                w-full py-2 px-2 border border-gray-300 placeholder:text-sm"
+                w-full py-2 px-2 border border-gray-200 placeholder:text-sm rounded-lg"
               />
             </label>
 
@@ -81,9 +80,8 @@ export default function VideoUploadPopup({ setShowPopup }) {
               <textarea
                 name="description"
                 type="text"
-                placeholder="description of the video goes here.."
                 className="bg-gray-100
-                h-30 w-full p-2 border border-gray-300 text-sm"
+                h-30 w-full p-2 border border-gray-200 text-sm rounded-lg"
               />
             </label>
           </div>
