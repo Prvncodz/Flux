0c9ba64be138d91919af8bb9b1ef5faf1a0a66b5
Prvncodz <praveenwks@gmail.com>
2026-03-16 07:36:37 +0530

    made home page and nav responsive

--- client/components/home/home.jsx
+++ client/components/home/home.jsx
@@ -6,35 +6,37 @@ import TweetFeed from "./tweetfeed/tweetFeed.jsx"
 import { useLocation } from "react-router-dom";
 
 export default function Home() {
-  const [isHomeSelected, setIsHomeSelected] = useState(true);
-  const values = { isHomeSelected, setIsHomeSelected };
-  const location = useLocation();
-  const { tab } = location.state || {}
+	const [isHomeSelected, setIsHomeSelected] = useState(true);
+	const values = { isHomeSelected, setIsHomeSelected };
+	const location = useLocation();
+	const { tab } = location.state || {}
 
-  useEffect(() => {
-    if (tab === "posts") {
-      setIsHomeSelected(false);
-    } else {
-      setIsHomeSelected(true);
-    }
-  }, [tab])
-  return (
-    <div className="relative z-0 flex items-center flex-col h-screen w-screen top-0 left-0 ">
-      <TabContext.Provider value={values}>
-        {
-          isHomeSelected ? (
-            <>
-              <Nav searchType={"video"} />
-              <Feed className="relative z-0" fetchType="all" />
-            </>
-          ) : (
-            <>
-              <Nav searchType={"post"} />
-              <TweetFeed className="relative z-0" />
-            </>
-          )
-        }
-      </TabContext.Provider>
-    </div>
-  );
+	useEffect(() => {
+		if (tab === "posts") {
+			setIsHomeSelected(false);
+		} else {
+			setIsHomeSelected(true);
+		}
+	}, [tab])
+	return (
+		<div className="relative z-0 flex items-center flex-col h-screen w-screen top-0 left-0 ">
+			<TabContext.Provider value={values}>
+				{
+					isHomeSelected ? (
+						<>
+							<Nav searchType={"video"} />
+							<div className="block">
+								<Feed className="relative z-0" fetchType="all" />
+							</div>
+						</>
+					) : (
+						<>
+							<Nav searchType={"post"} />
+							<TweetFeed className="relative z-0" />
+						</>
+					)
+				}
+			</TabContext.Provider>
+		</div>
+	);
 }
