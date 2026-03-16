4510fe90e49d2b46c2310344279a43b8dc56139b
Prvncodz <praveenwks@gmail.com>
2026-03-16 17:12:29 +0530

    made the liked videos page responsive

--- client/components/likedVideosPage/likedVideos.jsx
+++ client/components/likedVideosPage/likedVideos.jsx
@@ -6,164 +6,163 @@ import { useGetUserById } from "../../hooks/useGetUserById.jsx";
 import UserContext from "../../contexts/UserContext.jsx";
 
 export default function LikedVideos() {
-  const navigate = useNavigate();
-  const [videos, setVideos] = useState([]);
-  const { user, isUserLogged } = useContext(UserContext);
-  useEffect(() => {
-    async function getVideos() {
-      try {
-        const res = await axios.get(`/likes/videos${isUserLogged ? `?userId=${user._id}` : ``}`);
-        if (res.status === 200) {
-          setVideos(res.data?.data);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getVideos();
-  }, [])
-  return (
-    <>
-      <Nav />
-      <div className="max-w-lg   p-5 h-[95vh] overflow-y-auto rounded-xl">
+	const navigate = useNavigate();
+	const [videos, setVideos] = useState([]);
+	const { user, isUserLogged } = useContext(UserContext);
+	useEffect(() => {
+		async function getVideos() {
+			try {
+				const res = await axios.get(`/likes/videos${isUserLogged ? `?userId=${user._id}` : ``}`);
+				if (res.status === 200) {
+					setVideos(res.data?.data);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		getVideos();
+	}, [])
+	return (
+		<>
+			<Nav />
+			<div className="max-w-full   p-5 h-[95vh] overflow-y-auto rounded-xl md:max-w-full md:ml-20 md:p-5 ">
 
 
-        {videos.length === 0 ? (
-          <div className="flex flex-col items-center justify-center mt-20 gap-4 text-center">
-            <p className="text-gray-500">
-              You haven't liked any video yet
-            </p>
+				{videos.length === 0 ? (
+					<div className="flex flex-col items-center justify-center mt-20 gap-4 text-center">
+						<p className="text-gray-500">
+							You haven't liked any video yet
+						</p>
 
-            <button
-              onClick={() => navigate("/")}
-              className="bg-black text-white px-4 py-2 rounded-lg"
-            >
-              Explore Videos
-            </button>
-          </div>
-        ) : (
-          <div className="space-y-3">
-            <h1 className="text-2xl text-left font-semibold mb-6 mt-5">Liked Videos</h1>
-            {videos.map((video) => (
-              <VideoCardComponent video={video} key={video._id} />
-            ))}
-          </div>
-        )}
-      </div>
-    </>
-  );
+						<button
+							onClick={() => navigate("/")}
+							className="bg-black text-white px-4 py-2 rounded-lg"
+						>
+							Explore Videos
+						</button>
+					</div>
+				) : (
+					<div className="space-y-3">
+						<h1 className="text-2xl text-left font-semibold mb-6 mt-5 ">Liked Videos</h1>
+						{videos.map((video) => (
+							<VideoCardComponent video={video} key={video._id} />
+						))}
+					</div>
+				)}
+			</div>
+		</>
+	);
 }
 
 function VideoCardComponent({ video }) {
-  const { avatarUrl, fullname, username } = useGetUserById(video.owner);
-  const [duration, setDuration] = useState("00:00");
-  const [timeOfUpload, setTimeOfUpload] = useState("1 day");
-  const navigate = useNavigate();
+	const { avatarUrl, fullname, username } = useGetUserById(video.owner);
+	const [duration, setDuration] = useState("00:00");
+	const [timeOfUpload, setTimeOfUpload] = useState("1 day");
+	const navigate = useNavigate();
 
-  useEffect(() => {
+	useEffect(() => {
 
-    function calcDuration(dur) {
-      if (!dur) return;
-      const hours = Math.trunc(dur / 3600);
-      const minutes = Math.trunc((dur % 3600) / 60);
-      const seconds = Math.trunc((dur % 3600) % 60);
+		function calcDuration(dur) {
+			if (!dur) return;
+			const hours = Math.trunc(dur / 3600);
+			const minutes = Math.trunc((dur % 3600) / 60);
+			const seconds = Math.trunc((dur % 3600) % 60);
 
-      if (hours >= 1) {
-        setDuration(`${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`);
-      } else if (minutes >= 1) {
-        setDuration(`${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`);
-      } else {
-        setDuration(`00:${String(seconds).padStart(2, "0")}`);
-      }
-    }
-    function calcTimeOfUpload(t) {
-      if (!t) return;
-      const now = new Date();
-      const dif = now.getTime() - new Date(t).getTime();
-      const hours = Math.floor(dif / (1000 * 60 * 60));
-      const minutes = Math.floor(dif / 1000 * 60);
-      const days = Math.floor(dif / (1000 * 60 * 60 * 24));
-      const months = Math.floor(days / 30);
-      const years = Math.floor(days / 365);
-      const seconds = Math.floor(dif / 1000);
+			if (hours >= 1) {
+				setDuration(`${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`);
+			} else if (minutes >= 1) {
+				setDuration(`${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`);
+			} else {
+				setDuration(`00:${String(seconds).padStart(2, "0")}`);
+			}
+		}
+		function calcTimeOfUpload(t) {
+			if (!t) return;
+			const now = new Date();
+			const dif = now.getTime() - new Date(t).getTime();
+			const hours = Math.floor(dif / (1000 * 60 * 60));
+			const minutes = Math.floor(dif / 1000 * 60);
+			const days = Math.floor(dif / (1000 * 60 * 60 * 24));
+			const months = Math.floor(days / 30);
+			const years = Math.floor(days / 365);
+			const seconds = Math.floor(dif / 1000);
 
-      if (years > 0) {
-        setTimeOfUpload(`${years} ${years > 1 ? 'years' : 'year'}`);
-      } else if (months > 0) {
-        setTimeOfUpload(`${months} ${months > 1 ? 'months' : 'month'}`);
-      } else if (days > 0) {
-        setTimeOfUpload(`${days} ${days > 1 ? 'days' : 'day'}`);
-      } else if (hours > 0) {
-        setTimeOfUpload(`${hours} ${hours > 1 ? 'hours' : 'hour'}`);
-      } else if (minutes > 0) {
-        setTimeOfUpload(`${minutes} ${minutes > 1 ? 'minutes' : 'minute'}`);
-      } else {
-        setTimeOfUpload(`${seconds} ${seconds > 1 ? 'seconds' : 'second'}`)
-      }
-    }
-    calcTimeOfUpload(video.createdAt);
-    calcDuration(Math.trunc(video.duration));
-  }, [video])
+			if (years > 0) {
+				setTimeOfUpload(`${years} ${years > 1 ? 'years' : 'year'}`);
+			} else if (months > 0) {
+				setTimeOfUpload(`${months} ${months > 1 ? 'months' : 'month'}`);
+			} else if (days > 0) {
+				setTimeOfUpload(`${days} ${days > 1 ? 'days' : 'day'}`);
+			} else if (hours > 0) {
+				setTimeOfUpload(`${hours} ${hours > 1 ? 'hours' : 'hour'}`);
+			} else if (minutes > 0) {
+				setTimeOfUpload(`${minutes} ${minutes > 1 ? 'minutes' : 'minute'}`);
+			} else {
+				setTimeOfUpload(`${seconds} ${seconds > 1 ? 'seconds' : 'second'}`)
+			}
+		}
+		calcTimeOfUpload(video.createdAt);
+		calcDuration(Math.trunc(video.duration));
+	}, [video])
 
-  function handleShowUserProfile() {
-    navigate("/userchannel", {
-      state: {
-        otherUserName: username
-      }
-    });
-  }
-  function handleShowWatchVideo() {
-    navigate("/watch/video", {
-      state: {
-        videoId: video?._id,
-        ownerAvatar: avatarUrl,
-        username: username,
-      }
-    })
-  }
+	function handleShowUserProfile() {
+		navigate("/userchannel", {
+			state: {
+				otherUserName: username
+			}
+		});
+	}
+	function handleShowWatchVideo() {
+		navigate("/watch/video", {
+			state: {
+				videoId: video?._id,
+				ownerAvatar: avatarUrl,
+				username: username,
+			}
+		})
+	}
 
-  return (
-    <div
-      key={video._id}
-      className="flex gap-3 border border-neutral-200 rounded-xl p-3"
-      onClick={handleShowWatchVideo}
-    >
-      {/* thumbnail */}
-      <div className="relative">
-        <img
-          src={video.thumbnail?.url}
-          className="w-[142px] h-[84px] object-cover rounded-lg"
-        />
-        <div className="absolute bg-gray-800 rounded-2xl bottom-1 right-1 w-10 h-4 text-xs text-gray-300">
-          {duration}
-        </div>
-      </div>
+	return (
+		<div
+			className="flex gap-3 border border-neutral-200 rounded-xl p-3"
+			onClick={handleShowWatchVideo}
+		>
+			{/* thumbnail */}
+			<div className="relative">
+				<img
+					src={video.thumbnail?.url}
+					className="w-[142px] h-[84px] object-cover rounded-lg md:aspect-video md:h-30 md:w-50 lg:h-40 lg:w-70"
+				/>
+				<div className="absolute bg-gray-800 rounded-2xl bottom-1 right-1 w-10 h-4 text-xs text-gray-300">
+					{duration}
+				</div>
+			</div>
 
-      {/* video info */}
-      <div className="flex flex-col justify-between flex-1 h-[84px]">
-        <div className="text-left mt-1 ml-2">
-          <h3 className="text-sm font-semibold leading-tight line-clamp-2">
-            {video.title}
-          </h3>
+			{/* video info */}
+			<div className="flex flex-col justify-between flex-1 h-[84px] md:mt-1 md:p-y-20">
+				<div className="text-left mt-1 ml-2">
+					<h3 className="text-sm font-semibold leading-tight line-clamp-2 md:font-normal md:leading-relaxed md:text-lg  lg:font-normal lg:leading-relaxed lg:text-xl">
+						{video.title}
+					</h3>
 
-          <p className="text-xs text-gray-500">
-            {video.views + " views"} · {timeOfUpload + " ago"}
-          </p>
-        </div>
-        <div className="flex items-center gap-1  ml-2">
-          <img
-            src={avatarUrl}
-            className="w-6 h-6 rounded-full cursor-pointer"
-            onClick={handleShowUserProfile}
-          />
+					<p className="text-xs text-gray-500 md:text-sm  lg:text-base">
+						{video.views + " views"} · {timeOfUpload + " ago"}
+					</p>
+				</div>
+				<div className="flex items-center gap-1  ml-2 md:mt-3">
+					<img
+						src={avatarUrl}
+						className="w-6 h-6 rounded-full cursor-pointer lg:w-7 lg:h-7"
+						onClick={handleShowUserProfile}
+					/>
 
-          <span className="text-xs text-gray-600">
-            {fullname}
-          </span>
-        </div>
+					<span className="text-xs text-gray-600 md:ml-0.5 lg:text-sm">
+						{fullname}
+					</span>
+				</div>
 
-      </div>
-    </div>
+			</div>
+		</div>
 
-  );
+	);
 }
