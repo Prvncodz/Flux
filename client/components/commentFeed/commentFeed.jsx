78814448de9def3a8ec6182a0ad19b7babaaac37
Prvncodz <praveenwks@gmail.com>
2026-03-17 09:35:01 +0530

    ui changes

--- client/components/commentFeed/commentFeed.jsx
+++ client/components/commentFeed/commentFeed.jsx
@@ -2,103 +2,103 @@ import { useState, useEffect, useContext } from "react";
 import axios from "../../api/axios.js";
 import CommentComponent from "./commentComponent.jsx";
 import AddCommentsBox from "./AddCommentBox.jsx";
-import CrossIcon from "../assets/crossIcon.jsx";
 import UserContext from "../../contexts/UserContext.jsx";
+import { Cross } from "lucide-react";
 
 export default function CommentFeed({ fetchType, Id, isOpen, setIsOpen }) {
-  const [comments, setComments] = useState([{}]);
-  const [areCommentsFetched, SetAreCommentsFetched] = useState(false);
-  const { user } = useContext(UserContext);
-  useEffect(() => {
-    if (!Id) return;
-    async function fetchAllCommentsOnVideo(id) {
-      if (!id) return;
-      try {
-        await axios
-          .get(
-            `/comments/${id}/get-video-comments${user?._id ? `?userId=${user._id}` : ``}`,
-          )
-          .then((res) => {
-            setComments(res.data?.data);
-            SetAreCommentsFetched(true);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    async function fetchAllCommentsOnTweet(id) {
-      if (!id) return;
-      try {
-        await axios
-          .get(
-            `/comments/${id}/get-tweet-comments${user?._id ? `?userId=${user._id}` : ``}`,
-          )
-          .then((res) => {
-            setComments(res.data?.data);
-            SetAreCommentsFetched(true);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    async function fetchAllCommentsOnComment(id) {
-      if (!id) return;
-      try {
-        await axios
-          .get(
-            `/comments/${id}/get-comment-comments${user?._id ? `?userId=${user._id}` : ``}`,
-          )
-          .then((res) => {
-            setComments(res.data?.data);
-            SetAreCommentsFetched(true);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }
+	const [comments, setComments] = useState([{}]);
+	const [areCommentsFetched, SetAreCommentsFetched] = useState(false);
+	const { user } = useContext(UserContext);
+	useEffect(() => {
+		if (!Id) return;
+		async function fetchAllCommentsOnVideo(id) {
+			if (!id) return;
+			try {
+				await axios
+					.get(
+						`/comments/${id}/get-video-comments${user?._id ? `?userId=${user._id}` : ``}`,
+					)
+					.then((res) => {
+						setComments(res.data?.data);
+						SetAreCommentsFetched(true);
+					});
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		async function fetchAllCommentsOnTweet(id) {
+			if (!id) return;
+			try {
+				await axios
+					.get(
+						`/comments/${id}/get-tweet-comments${user?._id ? `?userId=${user._id}` : ``}`,
+					)
+					.then((res) => {
+						setComments(res.data?.data);
+						SetAreCommentsFetched(true);
+					});
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		async function fetchAllCommentsOnComment(id) {
+			if (!id) return;
+			try {
+				await axios
+					.get(
+						`/comments/${id}/get-comment-comments${user?._id ? `?userId=${user._id}` : ``}`,
+					)
+					.then((res) => {
+						setComments(res.data?.data);
+						SetAreCommentsFetched(true);
+					});
+			} catch (error) {
+				console.log(error);
+			}
+		}
 
-    if (fetchType === "tweet") {
-      fetchAllCommentsOnTweet(Id);
-    } else if (fetchType === "comment") {
-      fetchAllCommentsOnComment(Id);
-    } else {
-      fetchAllCommentsOnVideo(Id);
-    }
-  }, [fetchType, Id, user?.id]);
+		if (fetchType === "tweet") {
+			fetchAllCommentsOnTweet(Id);
+		} else if (fetchType === "comment") {
+			fetchAllCommentsOnComment(Id);
+		} else {
+			fetchAllCommentsOnVideo(Id);
+		}
+	}, [fetchType, Id, user?.id]);
 
-  return (
-    <>
-      <div
-        className="my-2 max-h-screen min-h-2.5 overflow-auto p-3 flex flex-col  rounded-2xl bg-gray-100 mx-2 ease-in-out relative z-0 ring ring-gray-100"
-        onClick={() => !isOpen && setIsOpen(true)}
-      >
-        <h1 className="text-gray-900 text-left text-lg text-medium">
-          {areCommentsFetched && comments.length} Comments
-        </h1>
-        {areCommentsFetched && isOpen ? (
-          <div>
-            <CrossIcon
-              size={26}
-              color={"#000000"}
-              className={"absolute top-4 right-4 z-1"}
-              onClick={() => isOpen && setIsOpen(false)}
-            />
-            <AddCommentsBox Id={Id} fetchType={fetchType} className={"mb-8"} />
-            {comments.map((comment, idx) => (
-              <CommentComponent key={idx} comment={comment} />
-            ))}
-          </div>
-        ) : areCommentsFetched && comments.length !== 0 ? (
-          <CommentComponent comment={comments[0]} onlyContent={true} />
-        ) : (
-          <div
-            className="my-3 h-auto flex p-3 rounded-2xl bg-gray-100 mx-2"
-            onClick={() => !isOpen && setIsOpen(true)}
-          >
-            No comments on this content yet
-          </div>
-        )}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className="my-2 max-h-screen min-h-2.5 overflow-auto p-3 flex flex-col  rounded-2xl bg-gray-100 mx-2 ease-in-out relative z-0 ring ring-gray-100"
+				onClick={() => !isOpen && setIsOpen(true)}
+			>
+				<h1 className="text-gray-900 text-left text-lg text-medium">
+					{areCommentsFetched && comments.length} Comments
+				</h1>
+				{areCommentsFetched && isOpen ? (
+					<div>
+						<Cross
+							size={26}
+							color={"#000000"}
+							className={"absolute top-4 right-4 z-1"}
+							onClick={() => isOpen && setIsOpen(false)}
+						/>
+						<AddCommentsBox Id={Id} fetchType={fetchType} className={"mb-8"} />
+						{comments.map((comment, idx) => (
+							<CommentComponent key={idx} comment={comment} />
+						))}
+					</div>
+				) : areCommentsFetched && comments.length !== 0 ? (
+					<CommentComponent comment={comments[0]} onlyContent={true} />
+				) : (
+					<div
+						className="my-3 h-auto flex p-3 rounded-2xl bg-gray-100 mx-2"
+						onClick={() => !isOpen && setIsOpen(true)}
+					>
+						No comments on this content yet
+					</div>
+				)}
+			</div>
+		</>
+	);
 }
--- client/components/userProfile/profile.jsx
+++ client/components/userProfile/profile.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useContext, useCallback } from "react";
 import UserContext from "../../contexts/UserContext.jsx";
 import axios from "../../api/axios.js";
-import ArrowLeft from "../assets/arrowLeft.jsx";
 import UserTick from "../assets/usertick.jsx";
 import UserAddIcon from "../assets/useradd.jsx";
 import dbanner from "../assets/dbanner.jpg";
@@ -10,221 +9,221 @@ import Button from "../button.jsx";
 import VideoFeed from "../home/videofeed/feed.jsx";
 import PostFeed from "../home/tweetfeed/tweetFeed.jsx";
 import PlaylistFeed from "../home/playlistfeed/playlistFeed.jsx";
-import ThreeDotsIcon from "../assets/moreDotsIcon.jsx";
 import EditProfilePopUp from "./editProfilePopup.jsx";
 import ChangePassPopup from "./changePass.jsx";
 import { useLocation } from "react-router-dom";
+import { ArrowLeftIcon, Ellipsis, LucideDotSquare } from "lucide-react";
 
 export default function Profile() {
-  const { user } = useContext(UserContext);
-  const [UserProfile, setUserProfile] = useState({});
-  const [tabOpened, setTabOpened] = useState("videos");
-  const [isPopupActive, setisPopupActive] = useState(false);
-  const [isEditPopUpActive, setIsEditPopUpActive] = useState(false);
-  const [isPassPopupActive, setIsPassPopupActive] = useState(false);
-  const [isOtherUserP, setIsOtherUserP] = useState(false);
-  const location = useLocation();
-  const { otherUserName } = location.state || {};
-  const [isSubscribed, setIsSubscribed] = useState(false);
-  const [isProfileFetched, setIsProfileFetched] = useState(false);
-  let timeoutId;
-  async function handleSubscription() {
-    clearTimeout(timeoutId);
-    setIsSubscribed(!isSubscribed);
-    timeoutId = setTimeout(async () => {
-      try {
-        await axios.post(`/subscriptions/c/${UserProfile?._id}`);
-      } catch (error) {
-        console.log(error);
-        console.log(error.response?.data?.message);
-      }
-    }, 800);
-  }
-  useEffect(() => {
-    if (otherUserName) {
-      if (otherUserName !== user?.userName) {
-        setIsOtherUserP(true);
-      }
-      if (!otherUserName) return;
-      async function getUserProfile(username) {
-        if (!username) return;
-        try {
-          const res = await axios.get(`/user/p/${username}`);
-          if (res.status === 200) {
-            setUserProfile(res.data?.data);
-            setIsSubscribed(res.data?.data?.isSubscribed);
-            setIsProfileFetched(true);
-            console.log(res.data?.data);
-          }
-        } catch (error) {
-          console.log(
-            "Error while fetching user's profile. err message",
-            error,
-          );
-        }
-      }
-      getUserProfile(otherUserName);
-    } else {
-      if (!user?.userName) return;
-      async function getUserProfile(username) {
-        if (!username) return;
-        try {
-          const res = await axios.get(`/user/p/${username}`);
-          if (res.status === 200) {
-            setUserProfile(res.data?.data);
-            setIsProfileFetched(true);
-          }
-        } catch (error) {
-          console.log(
-            "Error while fetching user's profile. err message",
-            error,
-          );
-        }
-      }
-      getUserProfile(user?.userName);
-    }
-  }, [user?.userName, otherUserName]);
+	const { user } = useContext(UserContext);
+	const [UserProfile, setUserProfile] = useState({});
+	const [tabOpened, setTabOpened] = useState("videos");
+	const [isPopupActive, setisPopupActive] = useState(false);
+	const [isEditPopUpActive, setIsEditPopUpActive] = useState(false);
+	const [isPassPopupActive, setIsPassPopupActive] = useState(false);
+	const [isOtherUserP, setIsOtherUserP] = useState(false);
+	const location = useLocation();
+	const { otherUserName } = location.state || {};
+	const [isSubscribed, setIsSubscribed] = useState(false);
+	const [isProfileFetched, setIsProfileFetched] = useState(false);
+	let timeoutId;
+	async function handleSubscription() {
+		clearTimeout(timeoutId);
+		setIsSubscribed(!isSubscribed);
+		timeoutId = setTimeout(async () => {
+			try {
+				await axios.post(`/subscriptions/c/${UserProfile?._id}`);
+			} catch (error) {
+				console.log(error);
+				console.log(error.response?.data?.message);
+			}
+		}, 800);
+	}
+	useEffect(() => {
+		if (otherUserName) {
+			if (otherUserName !== user?.userName) {
+				setIsOtherUserP(true);
+			}
+			if (!otherUserName) return;
+			async function getUserProfile(username) {
+				if (!username) return;
+				try {
+					const res = await axios.get(`/user/p/${username}`);
+					if (res.status === 200) {
+						setUserProfile(res.data?.data);
+						setIsSubscribed(res.data?.data?.isSubscribed);
+						setIsProfileFetched(true);
+						console.log(res.data?.data);
+					}
+				} catch (error) {
+					console.log(
+						"Error while fetching user's profile. err message",
+						error,
+					);
+				}
+			}
+			getUserProfile(otherUserName);
+		} else {
+			if (!user?.userName) return;
+			async function getUserProfile(username) {
+				if (!username) return;
+				try {
+					const res = await axios.get(`/user/p/${username}`);
+					if (res.status === 200) {
+						setUserProfile(res.data?.data);
+						setIsProfileFetched(true);
+					}
+				} catch (error) {
+					console.log(
+						"Error while fetching user's profile. err message",
+						error,
+					);
+				}
+			}
+			getUserProfile(user?.userName);
+		}
+	}, [user?.userName, otherUserName]);
 
-  const tabs = {
-    videos: <VideoFeed fetchType={"user"} userId={UserProfile?._id} />,
-    posts: <PostFeed fetchType={"user"} userId={UserProfile?._id} />,
-    playlists: <PlaylistFeed userId={UserProfile?._id} />,
-  };
+	const tabs = {
+		videos: <VideoFeed fetchType={"user"} userId={UserProfile?._id} />,
+		posts: <PostFeed fetchType={"user"} userId={UserProfile?._id} />,
+		playlists: <PlaylistFeed userId={UserProfile?._id} />,
+	};
 
-  return (
-    <>
-      <nav className="bg-neutral-100 h-12 w-full border-b-neutral-500 relative flex items-center justify-between ">
-        <a
-          href="/"
-          className="relative left-8/100 text-gray-800 font flex items-center justify-left"
-        >
-          <ArrowLeft />
-        </a>
-        <button
-          type=""
-          className="mr-3 relative"
-          onClick={() => setisPopupActive((prev) => !prev)}
-        >
-          <ThreeDotsIcon size={30} />
-        </button>
-        {isPopupActive && (
-          <>
-            <ul className="absolute top-12 right-0 w-45 h-auto  bg-neutral-100 shadow-xs z-10">
-              <li
-                className="text-gray-800 font-normal text-base p-2 border-b  border-neutral-400"
-                onClick={() => {
-                  setIsEditPopUpActive((prev) => !prev);
-                  setisPopupActive(false);
-                }}
-              >
-                Edit Profile
-              </li>
-              <li
-                className="text-gray-800 font-normal text-base p-2 border-b  border-neutral-400"
-                onClick={() => {
-                  setIsPassPopupActive(true);
-                  setisPopupActive(false);
-                }}
-              >
-                Change Password
-              </li>
-            </ul>
-          </>
-        )}
-      </nav>
-      {(isEditPopUpActive && (
-        <EditProfilePopUp setIsEditPopUpActive={setIsEditPopUpActive} />
-      )) ||
-        (isPassPopupActive && (
-          <ChangePassPopup setIsPassPopupActive={setIsPassPopupActive} />
-        ))}
-      <div className="relative h-45  z-0">
-        <img
-          src={UserProfile?.coverImage?.url || dbanner}
-          onError={(e) => (e.target.src = dbanner)}
-          className="h-full w-full relative"
-          loading="lazy"
-        />
-        <img
-          src={UserProfile?.avatar?.url || dpfp}
-          onError={(e) => (e.target.src = dpfp)}
-          className="h-20 rounded-full absolute left-1 -bottom-15 w-20 border-2 border-white"
-          loading="lazy"
-        />
-      </div>
-      <div className="flex justify-between h-auto w-full relative z-0">
-        <span className="ml-24 h-6">
-          <h3 className="text-left text-neutral-700 font-medium text-lg">
-            {UserProfile?.fullName || "Jhon doe"}
-          </h3>
-          <h3 className="text-left text-neutral-600 font-medium text-xs mt-0">
-            @{UserProfile?.userName || "jhondoe201"}
-          </h3>
-          <div className="flex">
-            <h3 className="text-left text-neutral-600 font-medium text-xs mt-0">
-              {UserProfile?.subscriberCount || "0"} Subscribers
-            </h3>
-            <span className="mx-1 align-middle text-gray-600">•</span>
-            <h3 className="text-left text-neutral-600 font-medium text-xs mt-0">
-              {UserProfile?.channelsSubscribedCount || 0} Subscribed
-            </h3>
-          </div>
-        </span>
-        {isOtherUserP && (
-          <Button
-            children={
-              isSubscribed ? (
-                <>
-                  <UserTick />
-                  <span>Subscribed</span>
-                </>
-              ) : (
-                <>
-                  <UserAddIcon />
-                  <span>Subscribe</span>
-                </>
-              )
-            }
-            classes="mt-4 mr-2"
-            onClick={handleSubscription}
-          />
-        )}
-      </div>
-      <div
-        className={`flex flex-row w-full ${isOtherUserP ? `mt-4` : `mt-12`} pb-2`}
-      >
-        <span
-          name="videos"
-          className={`text-base relative mt-3.5 font-normal w-50 cursor-pointer ${tabOpened === "videos" ? `text-[#1E549D]` : `text-gray-800 `}`}
-          onClick={() => setTabOpened("videos")}
-        >
-          Videos
-          <div
-            className={`absolute -bottom-2 w-full h-1 ${tabOpened === "videos" ? `bg-[#1E549D]` : ``}`}
-          ></div>
-        </span>
-        <span
-          name="posts"
-          className={`text-base relative mt-3.5 font-normal w-50 cursor-pointer ${tabOpened === "posts" ? `text-[#1E549D]` : `text-gray-800 `}`}
-          onClick={() => setTabOpened("posts")}
-        >
-          Posts
-          <div
-            className={`absolute -bottom-2 w-full h-1 ${tabOpened === "posts" ? `bg-[#1E549D]` : ``}`}
-          ></div>
-        </span>
-        <span
-          name="playlists"
-          className={`text-base relative mt-3.5 font-normal w-50 cursor-pointer ${tabOpened === "playlists" ? `text-[#1E549D]` : `text-gray-800 `}`}
-          onClick={() => setTabOpened("playlists")}
-        >
-          Playlists
-          <div
-            className={`absolute -bottom-2 w-full h-1 ${tabOpened === "playlists" ? `bg-[#1E549D]` : ``}`}
-          ></div>
-        </span>
-      </div>
-      <div>{isProfileFetched && tabs[tabOpened]}</div>
-    </>
-  );
+	return (
+		<>
+			<nav className="bg-neutral-50 h-12 w-full border-b-neutral-100 relative flex items-center justify-between ">
+				<a
+					href="/"
+					className="relative ml-5 text-gray-800 font flex items-center justify-left md:"
+				>
+					<ArrowLeftIcon />
+				</a>
+				<button
+					type=""
+					className="mr-3 relative"
+					onClick={() => setisPopupActive((prev) => !prev)}
+				>
+					<Ellipsis size={30} />
+				</button>
+				{isPopupActive && (
+					<>
+						<ul className="absolute top-12 right-0 w-45 h-auto  bg-neutral-50 shadow-xs z-10 p-3 px-2 ">
+							<li
+								className="text-gray-800 font-normal text-base p-2 rounded-2xl hover:bg-neutral-100 "
+								onClick={() => {
+									setIsEditPopUpActive((prev) => !prev);
+									setisPopupActive(false);
+								}}
+							>
+								Edit Profile
+							</li>
+							<li
+								className="text-gray-800 font-normal text-base p-2 rounded-2xl hover:bg-neutral-100"
+								onClick={() => {
+									setIsPassPopupActive(true);
+									setisPopupActive(false);
+								}}
+							>
+								Change Password
+							</li>
+						</ul>
+					</>
+				)}
+			</nav>
+			{(isEditPopUpActive && (
+				<EditProfilePopUp setIsEditPopUpActive={setIsEditPopUpActive} />
+			)) ||
+				(isPassPopupActive && (
+					<ChangePassPopup setIsPassPopupActive={setIsPassPopupActive} />
+				))}
+			<div className="relative h-45 z-0 md:h-60">
+				<img
+					src={UserProfile?.coverImage?.url || dbanner}
+					onError={(e) => (e.target.src = dbanner)}
+					className="h-full w-full relative"
+					loading="lazy"
+				/>
+				<img
+					src={UserProfile?.avatar?.url || dpfp}
+					onError={(e) => (e.target.src = dpfp)}
+					className="h-20 rounded-full absolute left-1 -bottom-15 w-20 border-2 border-white md:h-25 md:w-25 md:ml-2 "
+					loading="lazy"
+				/>
+			</div>
+			<div className="flex justify-between h-auto w-full relative z-0 md:ml-6 ">
+				<span className="ml-24 h-6">
+					<h3 className="text-left text-neutral-700 font-medium text-lg">
+						{UserProfile?.fullName || "Jhon doe"}
+					</h3>
+					<h3 className="text-left text-neutral-600 font-medium text-xs mt-0">
+						@{UserProfile?.userName || "jhondoe201"}
+					</h3>
+					<div className="flex">
+						<h3 className="text-left text-neutral-600 font-medium text-xs mt-0">
+							{UserProfile?.subscriberCount || "0"} Subscribers
+						</h3>
+						<span className="mx-1 align-middle text-gray-600">•</span>
+						<h3 className="text-left text-neutral-600 font-medium text-xs mt-0">
+							{UserProfile?.channelsSubscribedCount || 0} Subscribed
+						</h3>
+					</div>
+				</span>
+				{isOtherUserP && (
+					<Button
+						children={
+							isSubscribed ? (
+								<>
+									<UserTick />
+									<span>Subscribed</span>
+								</>
+							) : (
+								<>
+									<UserAddIcon />
+									<span>Subscribe</span>
+								</>
+							)
+						}
+						classes="mt-4 mr-2"
+						onClick={handleSubscription}
+					/>
+				)}
+			</div>
+			<div
+				className={`flex flex-row w-full ${isOtherUserP ? `mt-4` : `mt-12`} pb-2 justify-center`}
+			>
+				<div
+					name="videos"
+					className={`text-base relative mt-3.5 font-normal w-50 cursor-pointer text-center ${tabOpened === "videos" ? `text-[#1E549D]` : `text-gray-800 `}`}
+					onClick={() => setTabOpened("videos")}
+				>
+					Videos
+					<div
+						className={`absolute -bottom-2 w-full h-1 rounded-4xl ${tabOpened === "videos" ? `bg-[#1E549D]` : ``}`}
+					></div>
+				</div>
+				<div
+					name="posts"
+					className={`text-base relative mt-3.5 font-normal w-50 cursor-pointer text-center ${tabOpened === "posts" ? `text-[#1E549D]` : `text-gray-800 `}`}
+					onClick={() => setTabOpened("posts")}
+				>
+					Posts
+					<div
+						className={`absolute -bottom-2 w-full h-1 rounded-4xl ${tabOpened === "posts" ? `bg-[#1E549D]` : ``}`}
+					></div>
+				</div>
+				<div
+					name="playlists"
+					className={`text-base relative mt-3.5 font-normal w-50 cursor-pointer text-center ${tabOpened === "playlists" ? `text-[#1E549D]` : `text-gray-800 `}`}
+					onClick={() => setTabOpened("playlists")}
+				>
+					Playlists
+					<div
+						className={`absolute -bottom-2 w-full h-1 rounded-4xl ${tabOpened === "playlists" ? `bg-[#1E549D]` : ``}`}
+					></div>
+				</div>
+			</div>
+			<div>{isProfileFetched && tabs[tabOpened]}</div>
+		</>
+	);
 }
